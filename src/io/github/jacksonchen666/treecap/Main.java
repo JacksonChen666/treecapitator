4b566cf622f8aab40b34dfa44f8b059ef4ac38e4
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-09-12 13:19:44 +0200

    useless stuff

--- src/io/github/jacksonchen666/treecap/Main.java
+++ src/io/github/jacksonchen666/treecap/Main.java
@@ -5,7 +5,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        saveDefaultConfig();
         new TreeCap(this);
         getServer().getPluginManager().registerEvents(new Listener(), this);
         new MapCleanUp().runTaskTimer(this, 6000, 6000);
