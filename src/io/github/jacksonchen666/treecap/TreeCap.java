17b7ed100b092980d7027c1e7c2f700889c148eb
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-09-14 17:10:00 +0200

    repeating code moved

--- src/io/github/jacksonchen666/treecap/TreeCap.java
+++ src/io/github/jacksonchen666/treecap/TreeCap.java
@@ -80,54 +80,33 @@ public class TreeCap implements CommandExecutor, TabCompleter {
                         commandSender.sendMessage(warningMessage);
                     }
                     plugin.getConfig().set("settings.maxLogs", BreakingBlocks.maximum);
-                    try {
-                        plugin.getConfig().save(new File(plugin.getDataFolder(), "config.yml"));
-                    }
-                    catch (IOException e) {
-                        commandSender.sendMessage(ChatColors.color(getText("messages.save_failed")));
-                        e.printStackTrace();
-                    }
                     commandSender.sendMessage(ChatColors.color(getText("messages.set_maxLogs", prefix).replace("{amount}", String.valueOf(BreakingBlocks.maximum))));
                 }
                 else if (args[0].equalsIgnoreCase("cooldown")) {
                     BreakingBlocks.cooldown = number;
                     plugin.getConfig().set("settings.cooldown", BreakingBlocks.cooldown);
-                    try {
-                        plugin.getConfig().save(new File(plugin.getDataFolder(), "config.yml"));
-                    }
-                    catch (IOException e) {
-                        commandSender.sendMessage(ChatColors.color(getText("messages.save_failed")));
-                        e.printStackTrace();
-                    }
                     commandSender.sendMessage(ChatColors.color(getText("messages.set_cooldown", prefix).replace("{amount}", String.valueOf(BreakingBlocks.cooldown))));
                 }
                 else if (args[0].equalsIgnoreCase("blocksPerTick")) {
                     BreakingBlocks.blocksPerTick = number;
                     plugin.getConfig().set("settings.blocksPerTick", BreakingBlocks.blocksPerTick);
-                    try {
-                        plugin.getConfig().save(new File(plugin.getDataFolder(), "config.yml"));
-                    }
-                    catch (IOException e) {
-                        commandSender.sendMessage(ChatColors.color(getText("messages.save_failed")));
-                        e.printStackTrace();
-                    }
                     commandSender.sendMessage(ChatColors.color(getText("messages.set_blocksPerTick", prefix).replace("{amount}", String.valueOf(BreakingBlocks.blocksPerTick))));
                 }
                 else if (args[0].equalsIgnoreCase("searchTimeoutSeconds")) {
                     BreakingBlocks.searchTimeoutSeconds = number;
                     plugin.getConfig().set("settings.searchTimeoutSeconds", BreakingBlocks.searchTimeoutSeconds);
-                    try {
-                        plugin.getConfig().save(new File(plugin.getDataFolder(), "config.yml"));
-                    }
-                    catch (IOException e) {
-                        commandSender.sendMessage(ChatColors.color(getText("messages.save_failed")));
-                        e.printStackTrace();
-                    }
                     commandSender.sendMessage(ChatColors.color(getText("messages.set_searchTimeoutSeconds", prefix).replace("{amount}", String.valueOf(BreakingBlocks.searchTimeoutSeconds))));
                 }
                 else {
                     commandSender.sendMessage("Unknown setting.");
                     return false;
+                }
+                try {
+                    plugin.getConfig().save(new File(plugin.getDataFolder(), "config.yml"));
+                }
+                catch (IOException e) {
+                    commandSender.sendMessage(ChatColors.color(getText("messages.save_failed")));
+                    e.printStackTrace();
                 }
             }
             return true;
