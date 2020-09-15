7206d14ef3cc12e4f289ea618c89781f46c52848
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-09-15 21:05:30 +0200

    removal of useless stuff

--- src/io/github/jacksonchen666/treecap/exceptions/SearchTimeoutException.java
+++
@@ -1,7 +0,0 @@
-package io.github.jacksonchen666.treecap.exceptions;
-
-public class SearchTimeoutException extends RuntimeException {
-    public SearchTimeoutException(String message) {
-        super(message);
-    }
-}
--- src/io/github/jacksonchen666/treecap/utils/CustomItemManager.java
+++ src/io/github/jacksonchen666/treecap/utils/CustomItemManager.java
@@ -1,6 +1,5 @@
 package io.github.jacksonchen666.treecap.utils;
 
-import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -17,27 +16,35 @@ public class CustomItemManager {
         return item;
     }
 
-    public static ItemStack customItem(Player player, ItemStack item, String displayName, List<String> lore) {
-        ItemMeta meta = item.getItemMeta();
-        Objects.requireNonNull(meta).setDisplayName(displayName);
-        meta.setLore(lore);
-        item.setItemMeta(meta);
-        player.getInventory().addItem(item);
-        player.updateInventory();
-        return item;
-    }
+    // --Commented out by Inspection START (2020. 09. 15. 21:03:48:797):
+    //    public static ItemStack customItem(Player player, ItemStack item, String displayName, List<String> lore) {
+    //        ItemMeta meta = item.getItemMeta();
+    //        Objects.requireNonNull(meta).setDisplayName(displayName);
+    //        meta.setLore(lore);
+    //        item.setItemMeta(meta);
+    //        player.getInventory().addItem(item);
+    //        player.updateInventory();
+    //        return item;
+    //    }
+    // --Commented out by Inspection STOP (2020. 09. 15. 21:03:48:797)
 
-    public static boolean isCustomItem(ItemStack item) {
-        return item.hasItemMeta() && (Objects.requireNonNull(item.getItemMeta()).hasDisplayName() || item.getItemMeta().hasLore());
-    }
+    // --Commented out by Inspection START (2020. 09. 15. 21:04:01:474):
+    //    public static boolean isCustomItem(ItemStack item) {
+    //        return item.hasItemMeta() && (Objects.requireNonNull(item.getItemMeta()).hasDisplayName() || item.getItemMeta().hasLore());
+    //    }
+    // --Commented out by Inspection STOP (2020. 09. 15. 21:04:01:474)
 
-    public static boolean isCustomItem(ItemStack item, String displayName) {
-        return item.hasItemMeta() && Objects.requireNonNull(item.getItemMeta()).hasDisplayName() && item.getItemMeta().getDisplayName().equalsIgnoreCase(displayName);
-    }
+    // --Commented out by Inspection START (2020. 09. 15. 21:04:04:565):
+    //    public static boolean isCustomItem(ItemStack item, String displayName) {
+    //        return item.hasItemMeta() && Objects.requireNonNull(item.getItemMeta()).hasDisplayName() && item.getItemMeta().getDisplayName().equalsIgnoreCase(displayName);
+    //    }
+    // --Commented out by Inspection STOP (2020. 09. 15. 21:04:04:565)
 
-    public static boolean isCustomItem(ItemStack item, List<String> lore) {
-        return item.hasItemMeta() && Objects.requireNonNull(item.getItemMeta()).hasLore() && Objects.equals(item.getItemMeta().getLore(), lore);
-    }
+    // --Commented out by Inspection START (2020. 09. 15. 21:04:04:550):
+    //    public static boolean isCustomItem(ItemStack item, List<String> lore) {
+    //        return item.hasItemMeta() && Objects.requireNonNull(item.getItemMeta()).hasLore() && Objects.equals(item.getItemMeta().getLore(), lore);
+    //    }
+    // --Commented out by Inspection STOP (2020. 09. 15. 21:04:04:550)
 
     public static boolean isCustomItem(ItemStack item, String displayName, List<String> lore) {
         return item.hasItemMeta() && Objects.requireNonNull(item.getItemMeta()).hasDisplayName() && item.getItemMeta().hasLore() && item.getItemMeta().getDisplayName().equalsIgnoreCase(displayName) && Objects.equals(item.getItemMeta().getLore(), lore);
