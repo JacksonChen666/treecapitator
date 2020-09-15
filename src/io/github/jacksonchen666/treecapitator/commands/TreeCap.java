c844c5079c97a7b615635e3e13a4ea0bec7d6185
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-09-15 22:14:44 +0200

    change to not use a variable

--- src/io/github/jacksonchen666/treecapitator/commands/TreeCap.java
+++ src/io/github/jacksonchen666/treecapitator/commands/TreeCap.java
@@ -20,7 +20,7 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-@org.bukkit.plugin.java.annotation.command.Command(name = TreeCap.COMMAND_NAME, desc = "Get a Treecapitator, give others players, and settings.")
+@org.bukkit.plugin.java.annotation.command.Command(name = "treecapitator", desc = "Get a Treecapitator, give others players, and settings.")
 public class TreeCap implements CommandExecutor, TabCompleter {
     public static final String COMMAND_NAME = "treecapitator";
     public static final int dangerThreshold = 16384;
