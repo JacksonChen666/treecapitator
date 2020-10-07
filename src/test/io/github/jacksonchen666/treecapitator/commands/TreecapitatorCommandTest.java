0e7471d63d2f331feca6d5a2c06744a26fd63e46
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-10-07 09:02:22 +0200

    more tests

--- src/test/io/github/jacksonchen666/treecapitator/commands/TreecapitatorCommandTest.java
+++ src/test/io/github/jacksonchen666/treecapitator/commands/TreecapitatorCommandTest.java
@@ -5,12 +5,18 @@ import be.seeseemelk.mockbukkit.ServerMock;
 import be.seeseemelk.mockbukkit.command.CommandResult;
 import be.seeseemelk.mockbukkit.entity.PlayerMock;
 import io.github.jacksonchen666.treecapitator.Treecapitator;
+import io.github.jacksonchen666.treecapitator.utils.ChatColors;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
 import java.util.Objects;
+
+import static io.github.jacksonchen666.treecapitator.processings.BreakingBlocks.getBlocks;
 
 public class TreecapitatorCommandTest {
     private ServerMock server;
@@ -34,15 +40,30 @@ public class TreecapitatorCommandTest {
 
     @Test
     public void testOnCommand() {
-        CommandResult resultGive = server.executePlayer("treecapitator");
+        CommandResult resultGive = server.execute("treecapitator", player1);
+        resultGive.assertResponse(ChatColors.color("&a[&rTreecapitator&a]&r Ok, here is your treecapitator."));
         Assert.assertTrue(Objects.requireNonNull(player1.getInventory()).contains(TreecapitatorItem.createItem()));
 
-        CommandResult resultGiveOther = server.executePlayer("treecapitator", player1.getName());
+        CommandResult resultGiveOther = server.execute("treecapitator", player1, player2.getName());
+        resultGiveOther.assertResponse(ChatColors.color("&a[&rTreecapitator&a]&r Gave " + player2.getName() + " a treecapitator."));
         Assert.assertTrue(Objects.requireNonNull(player2.getInventory()).contains(TreecapitatorItem.createItem()));
 
-        CommandResult resultGiveNone = server.executePlayer("treecapitator", "not_a_name");
-        resultGiveNone.assertResponse("&a[&rTreecapitator&a]&r &cnot_a_name is not online!");
+        CommandResult resultGiveNone = server.execute("treecapitator", player1, "not_a_name");
+        resultGiveNone.assertResponse(ChatColors.color("&a[&rTreecapitator&a]&r &cnot_a_name is not online!"));
 
-        CommandResult resultTest = server.executePlayer("treecapitator", "test", "10");
+        CommandResult resultGetOpLess = server.execute("treecapitator", player2);
+        resultGetOpLess.assertResponse(ChatColors.color("&a[&rTreecapitator&a]&r  &cYou are missing permission: treecapitator.getItem"));
+
+        CommandResult resultGiveOpLess = server.execute("treecapitator", player2, player1.getName());
+        resultGiveOpLess.assertResponse(ChatColors.color("&a[&rTreecapitator&a]&r  &cYou are missing permission: treecapitator.giveItem"));
+
+        CommandResult resultGiveNoneOpLess = server.execute("treecapitator", player2, "not_a_name");
+        resultGiveNoneOpLess.assertResponse(ChatColors.color("&a[&rTreecapitator&a]&r  &cYou are missing permission: treecapitator.giveItem"));
+
+        server.executePlayer("treecapitator", "test", "10");
+        Block start = server.getWorlds().get(0).getBlockAt(0, 150, 0);
+        List<Block> blocks = getBlocks(start, 10);
+        blocks.removeIf(block -> block.getType() != Material.OAK_LOG);
+        Assert.assertTrue("Did not finish cutting logs down. " + blocks.size() + " blocks left uncut.", blocks.size() > 1);
     }
 }
