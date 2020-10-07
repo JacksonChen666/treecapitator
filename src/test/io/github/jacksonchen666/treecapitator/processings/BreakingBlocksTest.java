4a4f441a1915d74112cbf67f29d9ee126fb3149a
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-10-07 11:19:38 +0200

    cover more code with more testing

--- src/test/io/github/jacksonchen666/treecapitator/processings/BreakingBlocksTest.java
+++ src/test/io/github/jacksonchen666/treecapitator/processings/BreakingBlocksTest.java
@@ -3,6 +3,7 @@ package io.github.jacksonchen666.treecapitator.processings;
 import be.seeseemelk.mockbukkit.MockBukkit;
 import be.seeseemelk.mockbukkit.ServerMock;
 import io.github.jacksonchen666.treecapitator.Treecapitator;
+import org.bukkit.block.Block;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -10,19 +11,21 @@ import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class BreakingBlocksTest {
+    private ServerMock server;
 
     @Before
     public void setUp() {
-        ServerMock server = MockBukkit.mock();
+        server = MockBukkit.mock();
         server.addSimpleWorld("world");
         MockBukkit.load(Treecapitator.class);
     }
 
     @Test
-    public void testAcceptableBlock() {
+    public void testAcceptableMaterial() {
         List<Boolean> acceptables = Arrays.stream(BreakingBlocks.acceptableBlock).map(BreakingBlocks::acceptableBlock).collect(Collectors.toList());
         for (boolean bool : acceptables) {
             if (!bool) {
@@ -30,6 +33,13 @@ public class BreakingBlocksTest {
                 break;
             }
         }
+    }
+
+    @Test
+    public void testAcceptableBlock() {
+        Block block = Objects.requireNonNull(server.getWorld("world")).getBlockAt(0, 0, 0);
+        block.setType(BreakingBlocks.acceptableBlock[0]);
+        Assert.assertTrue(BreakingBlocks.acceptableBlock(block));
     }
 
     @After
